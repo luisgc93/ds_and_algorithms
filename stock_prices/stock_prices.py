1a58f0ed40c6cdd3596c9a9f64a1a0a7baa8e804
luisgc93 <luisgc93@gmail.com>
2022-03-12 21:40:53 -0500

    Add 1st problem with brute force implementation

---
+++ stock_prices/stock_prices.py
@@ -0,0 +1,26 @@
+"""
+Design an algorithm that determines the maximum profit that could
+have been made by buying and then selling a single share over a
+given day range, subject to the constraint that the buy and the
+sell have to take place at the start of the day. (This algorithm
+may be needed to backtest a trading strategy.)
+
+* If you try a few test cases, you will see that the  minimum can
+occur after the maximum, which violates the requirement in the
+problem statement-you have to buy before you can sell.
+"""
+
+from typing import List
+
+
+def calculate_highest_profit(start_prices: List[int]) -> int:
+    highest_profit = None
+    for i in range(len(start_prices)):
+        price = start_prices[i]
+        next_prices = start_prices[i:]
+        for j in range(len(next_prices)):
+            next_price = next_prices[j]
+            if highest_profit is None or next_price - price > highest_profit:
+                highest_profit = next_price - price
+
+    return highest_profit
---
+++ stock_prices/test_stock_prices.py
@@ -0,0 +1,13 @@
+import pytest
+
+from stock_prices.stock_prices import calculate_highest_profit
+
+
+@pytest.mark.parametrize("start_prices, expected_highest_profit", [
+    ([22, 25, 28, 27], 6),
+    ([22, 19, 22, 17], 3),
+    ([22, 19, 18, 12], 0),
+    ([20, 19, 18, 12, 13, 19, 11], 7),
+])
+def test_returns_highest_profit_from_start_prices_list(start_prices, expected_highest_profit):
+    assert calculate_highest_profit(start_prices) == expected_highest_profit
