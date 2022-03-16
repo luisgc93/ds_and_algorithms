e31ee0979d86541621d51d7eb98d3f5ac0348e13
luisgc93 <luisgc93@gmail.com>
2022-03-16 17:36:44 -0500

    add comment

--- primitive_types/primitive_types.py
+++ primitive_types/primitive_types.py
@@ -5,6 +5,21 @@ used to detect single bit errors in data storage and communication. It is fairly
 straightforward to write code that computes the parity of a single 64-bit word. How would
 you compute the parity of a very large number of 64-bit words?
 Hint: Use a lookup table, but don't use 264 entries!
+
+REMINDER - BITWISE OPERATORS:
+~ 1s COMPLEMENT (flip bits but remember about the sign bit - 0 for positive and 1 for
+negative), also 2s complement = 1s complement + 1)
+CONTINUE: https://www.bbc.co.uk/bitesize/guides/zjfgjxs/revision/5 - THEN WORK ON IMPROVING PARITY METHOD
+& BITWISE AND
+| BITWISE OR
+>> BIT SHIFT
+
+CONVERTING TO/FROM BINARY
+In [1]: bin(12)
+Out[1]: '0b1100'
+
+In [2]: int('0b1100', 2)
+Out[2]: 12
 """
 
 
