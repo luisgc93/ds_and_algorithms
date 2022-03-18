7a72727dbbdebf274b67ccab118670ab20027810
luisgc93 <luisgc93@gmail.com>
2022-03-17 21:11:31 -0500

    add failing test cases

--- primitive_types/test_primitive_types.py
+++ primitive_types/test_primitive_types.py
@@ -4,8 +4,8 @@ from primitive_types.primitive_types import compute_parity
 
 
 @pytest.mark.parametrize("number, expected_parity", [
-    (11, 1),
-    (136, 0)
+    (1, 1), (2, 0), (3, 1), (4, 1), (5, 0), (6, 1),
+    (7, 0), (8, 0), (9, 1), (10, 0), (11, 1), (136, 0)
 ])
 def test_returns_parity(number, expected_parity):
     assert compute_parity(number) == expected_parity
