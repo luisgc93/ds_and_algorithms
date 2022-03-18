0615822a4bceea1fd3d6f8fcd4bc5988d8693f4d
luisgc93 <luisgc93@gmail.com>
2022-03-17 20:50:50 -0500

    update doc string

--- primitive_types/primitive_types.py
+++ primitive_types/primitive_types.py
@@ -1,18 +1,16 @@
 """
-The parity of a binary word is 1 if the number of 1s in the word is odd; otherwise, it is 0.
-For example, the parity of 1011 is 1, and the parity of 10001000 is 0. Parity checks are
-used to detect single bit errors in data storage and communication. It is fairly
-straightforward to write code that computes the parity of a single 64-bit word. How would
-you compute the parity of a very large number of 64-bit words?
-Hint: Use a lookup table, but don't use 264 entries!
+*1 byte = 8 bits
+*In computing, a word is the natural unit of data used by a particular processor design
+(this varies and depends on architecture)
 
 REMINDER - BITWISE OPERATORS:
 ~ 1s COMPLEMENT (flip bits but remember about the sign bit - 0 for positive and 1 for
-negative), also 2s complement = 1s complement + 1)
-CONTINUE: https://www.bbc.co.uk/bitesize/guides/zjfgjxs/revision/5 - THEN WORK ON IMPROVING PARITY METHOD
-& BITWISE AND
-| BITWISE OR
->> BIT SHIFT
+negative - this means that a +ve number will always become -ve and vice versa)
+& BITWISE AND (check bits, write 1 if both are 1)
+| BITWISE OR (check bits, write 1 if any of them is 1)
+^ XOR ("Exclusive OR - check bits, write 1 if bits are different)
+>> RIGHT BIT SHIFT (remove bits on the left)
+>> LEFT BIT SHIFT (add 0s on the right)
 
 CONVERTING TO/FROM BINARY
 In [1]: bin(12)
@@ -20,6 +18,13 @@ Out[1]: '0b1100'
 
 In [2]: int('0b1100', 2)
 Out[2]: 12
+
+The parity of a binary word is 1 if the number of 1s in the word is odd; otherwise, it is 0.
+For example, the parity of 1011 is 1, and the parity of 10001000 is 0. Parity checks are
+used to detect single bit errors in data storage and communication. It is fairly
+straightforward to write code that computes the parity of a single 64-bit word. How would
+you compute the parity of a very large number of 64-bit words?
+Hint: Use a lookup table, but don't use 264 entries!
 """
 
 
