53f9c5faf0132172233898a8e9551f87014b6649
luisgc93 <luisgc93@gmail.com>
2022-03-24 08:59:46 -0500

    add failing test case

--- algorithms/test_two_sum.py
+++ algorithms/test_two_sum.py
@@ -9,6 +9,7 @@ from algorithms.two_sum import two_sum
     ([-3, 2, 0, 1], -3, [0, 2]),
     ([-3, 2, 0, 1, 5, 18, 11, 932], 20, [1, 5]),
     ([1, 1, 2, 0], 2, [0, 1]),
+    ([2, 5, 5, 11], 10, [1, 2]),
 ])
 def test_two_sum(nums, target, expected_answer):
     assert two_sum(nums, target) == expected_answer
