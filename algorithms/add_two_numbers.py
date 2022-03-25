7f64016dbdfc11f86cbc3b7275b98c97cb5e383e
luisgc93 <luisgc93@gmail.com>
2022-03-24 20:51:39 -0500

    add two numbers leetcode problem

--- algorithms/add_two_numbers.py
+++ algorithms/add_two_numbers.py
@@ -17,15 +17,40 @@ you don't have to shift the entire sequence unlike in a regular list.
 from typing import Optional
 
 
-class ListNode:
+class ListNode:  # Definition for singly-linked list.
     def __init__(self, val=0, next_item=None):
         self.val = val
         self.next = next_item
 
 
 def add_two_numbers(l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-    number_1 = 0
-    number_2 = 0
-    for digit in l1.val:
-        number_1 += digit
-    breakpoint()
+    """
+    https://www.youtube.com/watch?v=IkuWJSYqFAw
+    """
+    added = ListNode(val=(l1.val + l2.val) % 10)
+    carry_over = (l1.val + l2.val) // 10  # https://www.codingem.com/python-floor-division/
+    current_node = added
+
+    while l1.next and l2.next:
+        l1 = l1.next
+        l2 = l2.next
+        current_node.next = ListNode(val=(carry_over + l1.val + l2.val) % 10)
+        carry_over = (carry_over + l1.val + l2.val) // 10
+        current_node = current_node.next
+
+    while l1.next:
+        l1 = l1.next
+        current_node.next = ListNode(val=(carry_over + l1.val) % 10)
+        carry_over = (carry_over + l1.val) // 10
+        current_node = current_node.next
+
+    while l2.next:
+        l2 = l2.next
+        current_node.next = ListNode(val=(carry_over + l2.val) % 10)
+        carry_over = (carry_over + l2.val) // 10
+        current_node = current_node.next
+
+    if carry_over > 0:
+        current_node.next = ListNode(val=1)
+
+    return added
--- tests/algorithms/test_add_two_numbers.py
+++ tests/algorithms/test_add_two_numbers.py
@@ -1,13 +1,37 @@
+from typing import List
+
 import pytest
 
-from algorithms.add_two_numbers import add_two_numbers, ListNode
+from algorithms.add_two_numbers import ListNode, add_two_numbers
+
+
+def _convert_node_to_list(node: ListNode) -> List:
+    result = []
+    if node.next is None:
+        return [node.val]
+
+    while node.next is not None:
+        result.append(node.val)
+        node = node.next
+    return result
 
 
 @pytest.mark.parametrize("l1, l2, expected", [
-    ([2, 4, 3], [5, 6, 4], [7, 0, 8]),
-    ([0], [0], [0]),
-    ([9, 9, 9, 9, 9, 9, 9], [9, 9, 9, 9], [8, 9, 9, 9, 0, 0, 0, 1]),
+    (ListNode(val=2, next_item=ListNode(val=4, next_item=ListNode(val=3))),  # l1
+     ListNode(val=5, next_item=ListNode(val=6, next_item=ListNode(val=4))),  # l2
+     ListNode(val=7, next_item=ListNode(val=0, next_item=ListNode(8)))  # expected
+     ),
+    (ListNode(val=0),  # l1
+     ListNode(val=0),  # l2
+     ListNode(val=0)  # expected
+     ),
+    (ListNode(val=4, next_item=ListNode(val=2, next_item=ListNode(val=1))),  # l1
+     ListNode(val=5, next_item=ListNode(val=9)),  # l2
+     ListNode(val=9, next_item=ListNode(val=1, next_item=ListNode(2)))  # expected
+     ),
 ])
 def test_add_two_numbers(l1, l2, expected):
+    result = add_two_numbers(l1, l2)
 
-    assert add_two_numbers(ListNode(l1), ListNode(l2)) == ListNode(expected)
+    assert _convert_node_to_list(result) == _convert_node_to_list(expected)
+
