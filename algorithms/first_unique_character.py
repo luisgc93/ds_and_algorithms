c8720c6c284221120b502667962852276b750251
luisgc93 <luisgc93@gmail.com>
2022-05-07 23:11:59 +0400

    add improved performance version of first character problem

--- algorithms/first_unique_character.py
+++ algorithms/first_unique_character.py
@@ -1,14 +1,29 @@
 """
-Return the 1-based index of the first character that has a
-single occurrence in a string
+Return the 1-based index of the first character that has a single occurrence in a string
 """
 
 
 def get_unique_character(input_string: str) -> int:
-    # TODO: improve performance
-    for index, letter in enumerate(input_string):
-        if input_string.count(letter) > 1:
+    for index, character in enumerate(input_string):
+        if input_string.count(character) > 1:
             continue
         else:
             return index + 1
+    return -1
+
+
+def get_unique_character_improved_performance(input_string: str) -> int:
+    """
+    Here we use a dictionary for better performance
+    https://towardsdatascience.com/faster-lookups-in-python-1d7503e9cd38
+    """
+    frequency = {}
+    for character in input_string:
+        if character not in frequency:
+            frequency[character] = 1
+        else:
+            frequency[character] += 1
+    for i in range(len(input_string)):
+        if frequency[input_string[i]] == 1:
+            return i + 1
     return -1
--- tests/algorithms/test_first_unique_character.py
+++ tests/algorithms/test_first_unique_character.py
@@ -1,16 +1,19 @@
 import pytest
-from algorithms.first_unique_character import get_unique_character
+from algorithms.first_unique_character import get_unique_character, get_unique_character_improved_performance
 
 
 @pytest.mark.parametrize("input_string, expected_output", [
     ("statistics", 3),
     ("hackthegame", 3),
+    ("people", 3),
 ])
 def test_first_unique_character(input_string, expected_output):
 
     assert get_unique_character(input_string) == expected_output
+    assert get_unique_character_improved_performance(input_string) == expected_output
 
 
 def test_first_unique_character_big_input():
     input_string = "a" + "s" * 60_000 + "z" + "a" + "b" * 160_000
-    assert get_unique_character(input_string) == 60_002
+
+    assert get_unique_character_improved_performance(input_string) == 60_002
