12dc226758e8cdbb6f26c9d3f7f5524d7a2e7a75
luisgc93 <luisgc93@gmail.com>
2022-05-07 22:47:25 +0400

    test with big input

--- tests/algorithms/test_first_unique_character.py
+++ tests/algorithms/test_first_unique_character.py
@@ -10,3 +10,7 @@ def test_first_unique_character(input_string, expected_output):
 
     assert get_unique_character(input_string) == expected_output
 
+
+def test_first_unique_character_big_input():
+    input_string = "a" + "s" * 60_000 + "z" + "a" + "b" * 160_000
+    assert get_unique_character(input_string) == 60_002
