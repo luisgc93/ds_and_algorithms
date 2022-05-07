4b5befe6029d791941d9f655988a3595dfb54344
luisgc93 <luisgc93@gmail.com>
2022-05-07 18:26:31 +0200

    add failing test case

--- tests/algorithms/test_stock_prices.py
+++ tests/algorithms/test_stock_prices.py
@@ -7,6 +7,7 @@ from algorithms.stock_prices import calculate_highest_profit
     ([22, 25, 28, 27], 6),
     ([22, 19, 22, 17], 3),
     ([22, 19, 18, 12], 0),
+    ([1, 2, 2, 100], 95),  # failing test case
     ([20, 19, 18, 12, 13, 19, 11], 7),
 ])
 def test_returns_highest_profit_from_start_prices_list(start_prices, expected_highest_profit):
