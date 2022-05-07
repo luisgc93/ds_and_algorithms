c975354b981ceb726fa34aea3696e3ca6f503dc4
luisgc93 <luisgc93@gmail.com>
2022-05-08 00:57:22 +0400

    update stock prices

--- algorithms/stock_prices.py
+++ algorithms/stock_prices.py
@@ -14,15 +14,57 @@ problem statement-you have to buy before you can sell.
 from typing import List
 
 
-def calculate_highest_profit(start_prices: List[int]) -> int:
-    # TODO: fix algorithm to handle buys at multiple prices
+def buy_and_sell_stock_once(prices: List[int]) -> int:
+    """
+    Computes highest possible profit when buying and selling a stock once
+    :param prices: list of prices for the stock
+    :return: highest possible profit
+    """
     highest_profit = 0
-    for i in range(len(start_prices)):
-        price = start_prices[i]
-        next_prices = start_prices[i:]
+    for i in range(len(prices)):
+        price = prices[i]
+        next_prices = prices[i:]
         for j in range(len(next_prices)):
             next_price = next_prices[j]
             if next_price - price > highest_profit:
                 highest_profit = next_price - price
 
     return highest_profit
+
+
+def buy_and_sell_stock_once_v2(prices: List[int]) -> int:
+    """
+    Proposed version in the book
+    """
+    # you can't sell on the 1st day since you don't own the stock yet!
+    min_price_so_far = float("inf")
+    max_profit = 0
+
+    for price in prices:
+        profit_sell_today = price - min_price_so_far
+        max_profit = max(max_profit, profit_sell_today)
+        min_price_so_far = min(min_price_so_far, price)
+
+    return max_profit
+
+
+def buy_and_sell_stock_twice(prices: List[int]) -> int:
+    """
+    Solution: https://www.youtube.com/watch?v=ixhHvr7PrU0
+    Computes highest possible profit when buying and selling a stock twice
+    :param prices: list of prices for the stock
+    :return: highest possible profit
+    """
+    # WIP
+    min_price_so_far = float("inf")
+    max_profit = 0
+    number_of_buys = 0
+    price_to_purchases_map = {}
+    for price in prices:
+        min_price_so_far = min(min_price_so_far, price)
+        profit_sell_today = price - min_price_so_far
+        if profit_sell_today > max_profit:
+            price_to_purchases_map[price] += 1
+            min_price_so_far = float("inf")
+    breakpoint()
+    return max_profit
--- tests/algorithms/test_stock_prices.py
+++ tests/algorithms/test_stock_prices.py
@@ -1,14 +1,23 @@
 import pytest
 
-from algorithms.stock_prices import calculate_highest_profit
+from algorithms.stock_prices import buy_and_sell_stock_once, buy_and_sell_stock_once_v2, buy_and_sell_stock_twice
 
 
-@pytest.mark.parametrize("start_prices, expected_highest_profit", [
+@pytest.mark.parametrize("prices, expected_highest_profit", [
     ([22, 25, 28, 27], 6),
     ([22, 19, 22, 17], 3),
     ([22, 19, 18, 12], 0),
     ([20, 19, 18, 12, 13, 19, 11], 7),
-    ([1, 2, 2, 100], 95),  # failing test case
+    ([1, 2, 100], 99),
+    ([310, 315, 275, 295, 260, 270, 290, 230, 255, 250], 30),
 ])
-def test_returns_highest_profit_from_start_prices_list(start_prices, expected_highest_profit):
-    assert calculate_highest_profit(start_prices) == expected_highest_profit
+def test_returns_highest_profit_when_buying_and_selling_one_time(prices, expected_highest_profit):
+    assert buy_and_sell_stock_once(prices) == expected_highest_profit
+    assert buy_and_sell_stock_once_v2(prices) == expected_highest_profit
+
+
+@pytest.mark.parametrize("prices, expected_highest_profit", [
+    ([1, 2, 100], 197),  # failing test case
+])
+def test_returns_highest_profit_when_buying_and_selling_two_times(prices, expected_highest_profit):
+    assert buy_and_sell_stock_twice(prices) == expected_highest_profit
