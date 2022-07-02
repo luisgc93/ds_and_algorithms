ea5f9fb4b921b05963b9789bdca8df999c15cde4
luisgc93 <luisgc93@gmail.com>
2022-07-01 19:13:11 -0500

    add test case

--- tests/algorithms/test_integer_to_roman.py
+++ tests/algorithms/test_integer_to_roman.py
@@ -29,7 +29,7 @@ def test_find_closest_number_that_has_its_own_char(number, result):
     (1, "I"), (2, "II"), (3, "III"), (4, "IV"), (5, "V"), (8, "VIII"), (9, "IX"),
     (10, "X"), (40, "XL"), (50, "L"), (90, "XC"), (100, "C"),
     (400, "CD"), (500, "D"), (900, "CM"), (1000, "M"), (1994, "MCMXCIV"),
-    (58, "LVIII"), (20, "XX"), (34, "XXXIV")
+    (58, "LVIII"), (20, "XX"), (34, "XXXIV"), (49, "XLIX")
 ])
 def test_converts_number_to_roman_numeral(number, result):
 
